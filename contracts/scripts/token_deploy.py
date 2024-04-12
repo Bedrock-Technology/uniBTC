27cfdc963ad11fbfb1c20473c629772492f4eac6
johnbedrock <john_bedrock@proton.me>
2024-04-12 16:55:05 +0800

    initial commit for uniBTC

---
+++ contracts/scripts/token_deploy.py
@@ -0,0 +1,18 @@
+from brownie import *
+from pathlib import Path
+
+import time
+import pytest
+
+def main():
+    deps = project.load(  Path.home() / ".brownie" / "packages" / config["dependencies"][0])
+    TransparentUpgradeableProxy = deps.TransparentUpgradeableProxy
+
+    owner = accounts.load('mainnet-owner')
+    deployer = accounts.load('mainnet-deployer')
+
+    uniBTC_impl = uniBTC.deploy({'from': deployer})
+    uniBTC_proxy = TransparentUpgradeableProxy.deploy(uniBTC_impl, deployer, b'', {'from': deployer})
+
+    transparent_uniBTC = Contract.from_abi("unBTC",uniBTC_proxy.address, uniBTC.abi)
+    transparent_uniBTC.initialize(owner, owner, {'from': owner})
