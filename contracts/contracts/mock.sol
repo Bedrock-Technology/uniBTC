48e3e26a8a535be9d839ebaeaa85fe6a2b368a97
johnbedrock <john_bedrock@proton.me>
2024-04-22 12:54:10 +0800

    add mock and holesky deployment

---
+++ contracts/contracts/mock.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract WBTC is ERC20, Ownable {
+    constructor()
+        ERC20("WBTC", "WBTC")
+        Ownable()
+    {
+        setMintable(msg.sender, true);
+    }
+
+    // @dev mintable group
+    mapping(address => bool) public mintableGroup;
+    modifier onlyMintableGroup() {
+        require(mintableGroup[msg.sender], "WBTC: not in mintable group");
+        _;
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 8;
+    }
+
+    /**
+     * @dev set or remove address to mintable group
+     */
+    function setMintable(address account, bool allow) public onlyOwner {
+        require(mintableGroup[account] != allow, "already set");
+        mintableGroup[account] = allow;
+    }
+
+    function mint(address to, uint256 amount) public onlyMintableGroup {
+        _mint(to, amount);
+    }
+}
