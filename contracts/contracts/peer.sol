2c1a28e599de0874d0126db56ce1ed7dced9fa20
Trisome-Bedrock <trisome@bedrock-technology>
2024-06-20 17:14:09 +0800

    add Peer contract to support cross-chain transfer

---
+++ contracts/contracts/peer.sol
@@ -0,0 +1,239 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@celer-network/contracts/message/framework/MessageApp.sol";
+import "../interfaces/iface.sol";
+
+contract Peer is MessageApp, Pausable, ReentrancyGuard, AccessControl {
+    using SafeERC20 for IERC20;
+    using Address for address payable;
+
+    bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+
+    struct Request {
+        address sender;
+        address recipient;
+        uint256 amount;
+        uint64 nonce;
+    }
+
+    /**
+     * @dev The minimum amount to make a cross-chain transfer.
+     */
+    uint256 public minTransferAmt = 0.02 ether;
+
+    /**
+    * @dev The local uniBTC ERC-20 token address.
+     */
+    address public immutable uniBTC;
+
+    /**
+     * @dev The map for configuring peers to chain ID.
+     */
+    mapping(uint64 => address) public peers;
+
+    /**
+     * @dev The map for recording request IDs and corresponding statuses.
+     *
+     * Key: Request ID, result of keccak256(abi.encodePacked(dstChainId, dstPeer, sender, recipient, amount, nonce)).
+     * Value: 1 indicates a cross-chain request has been sent, 2 indicates a refund has occurred.
+     */
+    mapping(bytes32 => uint8) public requestIds;
+
+    /** 
+     * @dev The counter to record each cross-chain transaction
+     */
+    uint64 public nonce;
+
+    receive() external payable { }
+
+    constructor(address _messageBus, address _uniBTC) MessageApp(_messageBus) {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(MANAGER_ROLE, msg.sender);
+        _setupRole(PAUSER_ROLE, msg.sender);
+
+        uniBTC = _uniBTC;
+    }
+
+
+    /**
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     * SENDER FUNCTIONS
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     */
+
+
+    /**
+     * @dev Burn uniBTC on the source chain and mint the corresponding amount of uniBTC on the destination chain
+     * for the given recipient.
+     *
+     * In the extremely rare case that assets have been burned on the source chain but eventually fail to be minted
+     * on the destination chain, the "crossTransferRefund" function should be called to restore the user's assets.
+     *
+     * There are several potential reasons for this extreme situation, including:
+     * 1. The destination peer is unable to receive the request due to an SGN routing problem.
+     * 2. Message execution reverts on the destination chain without a successful retry.
+     */
+    function crossTransfer(
+        uint64 _dstChainId,
+        address _recipient,
+        uint256 _amount
+    ) external payable whenNotPaused {
+        address dstPeer = peers[_dstChainId];
+
+        require(dstPeer != address(0), "destination peer does not exist");
+        require(_amount >= minTransferAmt, "invalid amount to transfer");
+        require(_recipient != address(0), "cross transfer to the zero address");
+
+        // Burn uniBTC
+        IMintableContract(uniBTC).burnFrom(msg.sender, _amount);
+
+        // Request to mint uniBTC
+        bytes memory message = abi.encode(
+            Request({sender: msg.sender, recipient: _recipient, amount: _amount, nonce: nonce})
+        );
+        sendMessage(
+            dstPeer,
+            _dstChainId,
+            message,
+            msg.value
+        );
+
+        // Update states
+        nonce++;
+        bytes32 reqId = keccak256(abi.encodePacked(_dstChainId, dstPeer, msg.sender, _recipient, _amount, nonce));
+        requestIds[reqId] = 1;
+        emit SrcBurned(_dstChainId, dstPeer, msg.sender, _recipient, _amount, nonce);
+    }
+
+    /**
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     * RECEIVER FUNCTIONS
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     */
+
+    /**
+     * @dev Called by MessageBus to execute a message to mint uniBTC on the destination chain.
+     */
+    function executeMessage(
+        address _srcPeer,
+        uint64 _srcChainId,
+        bytes calldata _message,
+        address // executor
+    ) external payable override onlyMessageBus returns (ExecutionStatus) {
+        require(_srcPeer != peers[_srcChainId], "illegal remote caller");
+
+        Request memory req = abi.decode((_message), (Request));
+
+        // Mint uniBTC
+        IMintableContract(uniBTC).mint(req.recipient, req.amount);
+        emit DstMinted(_srcChainId, _srcPeer, req.sender, req.recipient, req.amount, req.nonce);
+
+        return ExecutionStatus.Success;
+    }
+
+
+    /**
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     * ADMIN Functions
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     */
+
+    /**
+     * @dev Refund in the extremely rare case that assets have been burned on the source chain
+     * but fail to be minted on the destination chain.
+     */
+    function crossTransferRefund(
+        uint64 _dstChainId,
+        address _dstPeer,
+        address _sender,
+        address _recipient,
+        uint256 _amount,
+        uint256 _nonce
+    ) external onlyRole(MANAGER_ROLE) {
+        bytes32 reqId = keccak256(abi.encodePacked(_dstChainId, _dstPeer, _sender, _recipient, _amount, _nonce));
+        require(requestIds[reqId] == 1, "invalid refund inputs");
+
+        requestIds[reqId] == 2;
+        IMintableContract(uniBTC).mint(_sender, _amount);
+        emit SrcRefunded(_dstChainId, _dstPeer, _sender, _recipient, _amount, _nonce);
+    }
+
+    /**
+     * @dev Claim native tokens that are accidentally sent to this contract.
+     */
+    function claimLockedEthers(address _recipient, uint256 _amount) nonReentrant onlyRole(MANAGER_ROLE) external {
+        payable(_recipient).sendValue(_amount);
+        emit NativeTokensClaimed(_recipient, _amount);
+    }
+
+    /**
+     * @dev Claim ERC-20 tokens that are accidentally sent to this contract.
+     */
+    function claimLockedTokens(address _recipient, address _token, uint256 _amount) nonReentrant onlyRole(MANAGER_ROLE) external {
+        IERC20(_token).safeTransfer(_recipient, _amount);
+        emit ERC20TokensClaimed(_recipient, _token, _amount);
+    }
+
+    /**
+     * @dev Set the minimum amount to make a cross-chain transfer.
+     */
+    function setMinTransferAmt(uint256 _minimalAmt) external onlyRole(MANAGER_ROLE) {
+        minTransferAmt = _minimalAmt;
+        emit MinTransferAmtSet(_minimalAmt);
+    }
+
+    /**
+     * @dev Configure peers to chain ID so that they can communicate with each other and avoid illegal minting requests.
+     */
+    function configurePeers(uint64[] calldata _chainIds, address[] calldata _peers) external onlyRole(MANAGER_ROLE) {
+        require(_chainIds.length > 0 && _chainIds.length == _peers.length, "invalid input array lengths");
+
+        for (uint256 i = 0; i < _chainIds.length; i++) {
+            uint64 chainId = _chainIds[i];
+            address peer = _peers[i];
+
+            require(chainId != 0, "chain ID cannot be zero");
+            require(peer != address(0), "invalid peer address");
+
+            peers[chainId] = peer;
+        }
+
+        emit PeersConfigured(_chainIds, _peers);
+    }
+
+    /**
+     * @dev Pause the contract
+     */
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    /**
+     * @dev Unpause the contract
+     */
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    /**
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     * CONTRACT EVENTS
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     */
+    event SrcBurned(uint64 dstChainId, address dstPeer, address sender, address recipient, uint256 amount, uint256 nonce);
+    event DstMinted(uint64 srcChainId, address srcPeer, address sender, address recipient, uint256 amount, uint256 nonce);
+    event SrcRefunded(uint64 dstChainId, address dstPeer, address sender, address recipient, uint256 amount, uint256 nonce);
+
+    event NativeTokensClaimed(address recipient, uint256 amount);
+    event ERC20TokensClaimed(address recipient, address token, uint256 amount);
+
+    event MinTransferAmtSet(uint256 amount);
+    event PeersConfigured(uint64[] chainIds, address[] peers);
+}
+
