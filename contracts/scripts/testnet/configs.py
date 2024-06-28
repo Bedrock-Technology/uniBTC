859cacfdc2919b749c0b3e8b5b8816d5f8d22557
Trisome-Bedrock <trisome@bedrock-technology>
2024-06-28 14:32:55 +0800

    scripts/test: deploy contracts on avax-test, bsc-test and ftm-test

---
+++ contracts/scripts/testnet/configs.py
@@ -0,0 +1,30 @@
+# Reference: https://im-docs.celer.network/developer/contract-addresses-and-rpc-info#testnet
+
+contracts = {
+    "avax-test": {
+        "chain_id": 43113,
+        "message_bus": "0xE9533976C590200E32d95C53f06AE12d292cFc47",
+        "wbtc": "0x56c3024eB229Ca0570479644c78Af9D53472B3e4",
+        "uni_btc": "0x16221CaD160b441db008eF6DA2d3d89a32A05859",
+        "vault": "0x97e16DB82E089D0C9c37bc07F23FcE98cfF04823",
+        "peer": "0xC0c9E78BfC3996E8b68D872b29340816495D7e89"
+    },
+    "bsc-test": {
+        "chain_id": 97,
+        "message_bus": "0xAd204986D6cB67A5Bc76a3CB8974823F43Cb9AAA",
+        "wbtc": "0xC0c9E78BfC3996E8b68D872b29340816495D7e89",
+        "uni_btc": "0x16221CaD160b441db008eF6DA2d3d89a32A05859",
+        "vault": "0x97e16DB82E089D0C9c37bc07F23FcE98cfF04823",
+        "peer": "0xcBf3e6Ad1eeD0f3F81fCc2Ae76A0dB16C4e747B0"
+    },
+    "ftm-test": {
+        "chain_id": 4002,
+        "message_bus": "0xb92d6933A024bcca9A21669a480C236Cbc973110",
+        "wbtc": "0x56c3024eB229Ca0570479644c78Af9D53472B3e4",
+        "uni_btc": "0x16221CaD160b441db008eF6DA2d3d89a32A05859",
+        "vault": "0x97e16DB82E089D0C9c37bc07F23FcE98cfF04823",
+        "peer": "0xC0c9E78BfC3996E8b68D872b29340816495D7e89"
+    }
+}
+
+amount = 1e8
---
+++ contracts/scripts/testnet/deploy.py
@@ -0,0 +1,96 @@
+from brownie import WBTC, uniBTC, Vault, Peer, accounts, Contract, project, config
+from pathlib import Path
+from web3 import Web3
+
+from scripts.testnet.configs import contracts
+
+# Execution Command Format:
+# `brownie run scripts/testnet/deployment.py main "deployer" "owner" "avax-test" --network=avax-test`
+
+
+def main(deployer="deployer", owner="owner", network="avax-test"):
+    # Reference: https://docs.openzeppelin.com/contracts/4.x/api/proxy#TransparentUpgradeableProxy
+    deps = project.load(  Path.home() / ".brownie" / "packages" / config["dependencies"][0])
+    proxy = deps.TransparentUpgradeableProxy
+
+    w3 = Web3(Web3.HTTPProvider('http://localhost:8545'))
+    minter_role = w3.keccak(text='MINTER_ROLE')
+
+    message_bus = contracts[network]["message_bus"]
+
+    deployer = accounts.load(deployer)
+    owner = accounts.load(owner)
+
+    # Deploy contracts
+    wbtc = WBTC.deploy({'from': owner})
+
+    uni_btc = uniBTC.deploy({'from': deployer})
+    uni_btc_proxy = proxy.deploy(uni_btc, deployer, b'', {'from': deployer})
+    uni_btc_transparent = Contract.from_abi("uniBTC", uni_btc_proxy.address, uniBTC.abi)
+
+    vault = Vault.deploy({'from': deployer})
+    vault_proxy = proxy.deploy(vault, deployer, b'', {'from': deployer})
+    vault_transparent = Contract.from_abi("Vault", vault_proxy.address, Vault.abi)
+
+    peer = Peer.deploy(message_bus, uni_btc_transparent, {'from': owner})
+
+    # Initialize contracts
+    uni_btc_transparent.initialize(owner, owner, {'from': owner})
+    vault_transparent.initialize(owner, wbtc, uni_btc_transparent, {'from': owner})
+
+    # Grant MINTER_ROLE
+    minters = [vault_transparent, peer]
+    for minter in minters:
+        uni_btc_transparent.grantRole(minter_role, minter, {'from': owner})
+
+    # Check status
+    assert wbtc.mintableGroup(owner)
+
+    assert vault_transparent.WBTC() == wbtc
+    assert vault_transparent.uniBTC() == uni_btc_transparent
+
+    assert peer.messageBus() == message_bus
+    assert peer.uniBTC() == uni_btc_transparent
+
+    assert uni_btc_transparent.hasRole(minter_role, vault_transparent)
+    assert uni_btc_transparent.hasRole(minter_role, peer)
+
+    print("Deployed WBTC address: ", wbtc)
+    print("Deployed Peer address: ", peer)
+    print("Deployed uniBTC proxy address: ", uni_btc_transparent)
+    print("Deployed Vault proxy address: ", vault_transparent)
+
+    print("")
+
+    print("Deployed uniBTC address: ", uni_btc)
+    print("Deployed Vault address: ", vault)
+
+    # Deployed contracts on avax-test
+    # Deployed WBTC address:  0x56c3024eB229Ca0570479644c78Af9D53472B3e4
+    # Deployed Peer address:  0xC0c9E78BfC3996E8b68D872b29340816495D7e89
+    # Deployed uniBTC proxy address:  0x16221CaD160b441db008eF6DA2d3d89a32A05859
+    # Deployed Vault proxy address:  0x97e16DB82E089D0C9c37bc07F23FcE98cfF04823
+    #
+    # Deployed uniBTC address:  0x57518941854F879f80fA1ABF2366f54339DE8436
+    # Deployed Vault address:  0x236f8c0a61dA474dB21B693fB2ea7AAB0c803894
+
+    # Deployed contracts on bsc-test
+    # Deployed WBTC address:  0xC0c9E78BfC3996E8b68D872b29340816495D7e89
+    # Deployed Peer address:  0xcBf3e6Ad1eeD0f3F81fCc2Ae76A0dB16C4e747B0
+    # Deployed uniBTC proxy address:  0x16221CaD160b441db008eF6DA2d3d89a32A05859
+    # Deployed Vault proxy address:  0x97e16DB82E089D0C9c37bc07F23FcE98cfF04823
+    #
+    # Deployed uniBTC address: 0x57518941854F879f80fA1ABF2366f54339DE8436
+    # Deployed Vault address:  0x236f8c0a61dA474dB21B693fB2ea7AAB0c803894
+
+    # Deployed contracts on ftm-test
+    # Deployed WBTC address:  0x56c3024eB229Ca0570479644c78Af9D53472B3e4
+    # Deployed Peer address:  0xC0c9E78BfC3996E8b68D872b29340816495D7e89
+    # Deployed uniBTC proxy address:  0x16221CaD160b441db008eF6DA2d3d89a32A05859
+    # Deployed Vault proxy address:  0x97e16DB82E089D0C9c37bc07F23FcE98cfF04823
+    #
+    # Deployed uniBTC address:  0x57518941854F879f80fA1ABF2366f54339DE8436
+    # Deployed Vault address:  0x236f8c0a61dA474dB21B693fB2ea7AAB0c803894
+
+
+
