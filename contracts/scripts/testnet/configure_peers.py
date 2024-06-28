35cb19cac5b75f778f95a6e5203e8df770209ad1
Trisome-Bedrock <trisome@bedrock-technology>
2024-06-28 14:33:21 +0800

    scripts/test: configure peers

---
+++ contracts/scripts/testnet/configure_peers.py
@@ -0,0 +1,25 @@
+from brownie import Peer, accounts, Contract, project, config
+
+from scripts.testnet.configs import contracts
+
+
+def main(manager="manager", network="avax-test"):
+    manager = accounts.load(manager)
+
+    chain_ids = [contracts["avax-test"]["chain_id"],
+                 contracts["bsc-test"]["chain_id"],
+                 contracts["ftm-test"]["chain_id"]]
+
+    peers = [contracts["avax-test"]["peer"],
+             contracts["bsc-test"]["peer"],
+             contracts["ftm-test"]["peer"]]
+
+    peer = contracts[network]["peer"]
+
+    peer = Contract.from_abi("Peer", peer, Peer.abi)
+
+    tx = peer.configurePeers(chain_ids, peers, {'from': manager})
+    assert "PeersConfigured" in tx.events
+    assert peer.peers(contracts["avax-test"]["chain_id"]) == contracts["avax-test"]["peer"]
+    assert peer.peers(contracts["bsc-test"]["chain_id"]) == contracts["bsc-test"]["peer"]
+    assert peer.peers(contracts["ftm-test"]["chain_id"]) == contracts["ftm-test"]["peer"]
