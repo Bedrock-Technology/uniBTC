9915f4de01ec2e100e8f2f9063f22427bc9df11e
Trisome-Bedrock <trisome@bedrock-technology>
2024-07-02 18:00:21 +0800

    handle wrapped BTC with variant decimals

--- contracts/contracts/Vault.sol
+++ contracts/contracts/Vault.sol
@@ -20,38 +20,61 @@ contract Vault is Initializable, AccessControlUpgradeable, PausableUpgradeable, 
     mapping(address => uint256) public caps;
     mapping(address => bool) public paused;
 
+    uint private constant MULTIPLIER = 1e24;
+
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
     /**
      * @dev mint uniBTC with WBTC
+     * @param _amount the quantity of WBTC for exchange of uniBTC
+     * @return minted the quantity of minted uniBTC.
      */
-    function mint(uint256 _amount) external {
+    function mint(uint256 _amount) external returns (uint256 minted) {
         require(!paused[WBTC], "SYS003");
-        _mint(WBTC, _amount);
+        return _mint(WBTC, _amount);
     }
 
     /**
      * @dev mint uniBTC with the given type of wrapped BTC
+     * @param _token the wrapped BTC for exchange of uniBTC
+     * @param _amount the quantity of wrapped BTC for exchange of uniBTC
+     * @return minted the quantity of minted uniBTC.
      */
-    function mint(address _token, uint256 _amount) external {
+    function mint(address _token, uint256 _amount) external returns (uint256 minted) {
         require(!paused[_token], "SYS004");
-        _mint(_token, _amount);
+        return _mint(_token, _amount);
     }
 
     /**
      * @dev burn uniBTC and redeem WBTC
+     * @param _amount the quantity of WBTC to redeem
+     * @return burned the quantity of uniBTC burned
      */
-    function redeem(uint256 _amount) external {
-        _redeem(WBTC, _amount);
+    function redeem(uint256 _amount) external returns (uint256 burned) {
+        return _redeem(WBTC, _amount);
     }
 
     /**
      * @dev burn uniBTC and redeem the given type of wrapped BTC
+     * @param _token the wrapped BTC to redeem
+     * @param _amount the quantity wrapped BTC to redeem
+     * @return burned the quantity of uniBTC burned
      */
-    function redeem(address _token, uint256 _amount) external {
-        _redeem(_token, _amount);
+    function redeem(address _token, uint256 _amount) external returns (uint256 burned) {
+        return _redeem(_token, _amount);
+    }
+
+    /**
+     * @dev calculate the exchange ratio of uniBTC to xBTC (wrapped BTC), which conforms to the proportion `1 uniBTC = 1 xBTC`.
+     * To calculate the wrapped BTC amount equivalent to the specified uniBTC amount, use the following formula:
+     * xBTCAmt = uniBTCAmt * r / 1e24, where r stands for the result of exchangeRatio(xBTC).
+     */
+    function exchangeRatio(address _token) internal returns (uint256) {
+        uint256 xBTCDecimals = ERC20(_token).decimals();
+        uint256 uniBTCDecimals = ERC20(uniBTC).decimals();
+        return 10**xBTCDecimals * MULTIPLIER / 10**uniBTCDecimals;
     }
 
     /**
@@ -97,7 +120,7 @@ contract Vault is Initializable, AccessControlUpgradeable, PausableUpgradeable, 
      */
     function setCap(address _token, uint256 _cap) external onlyRole(DEFAULT_ADMIN_ROLE) {
         require(_token != address(0x0), "SYS005");
-        require(ERC20(_token).decimals() == 8, "SYS006");
+        require(_exchangeRatio(_token) > 0, "SYS006");
         caps[_token] = _cap;
     }
 
@@ -117,23 +140,48 @@ contract Vault is Initializable, AccessControlUpgradeable, PausableUpgradeable, 
      * ======================================================================================
      */
     /**
-     * @dev mint internal 
+     * @dev mint internal
      */
-    function _mint(address _token, uint256 _amount) internal {
+    function _mint(address _token, uint256 _amount) internal returns (uint256) {
         require(IERC20(_token).balanceOf(address(this)) + _amount <= caps[_token], "USR003");
 
         IERC20(_token).safeTransferFrom(msg.sender, address(this), _amount);
-        IMintableContract(uniBTC).mint(msg.sender, _amount);
+
+        uint256 uniBTCAmt = _uniBTCAmt(_token, _amount);
+        IMintableContract(uniBTC).mint(msg.sender, uniBTCAmt);
         emit Minted(_token, _amount);
+
+        return uniBTCAmt;
     }
 
     /**
      * @dev redeem internal
      */
-    function _redeem(address _token, uint256 _amount) internal {
+    function _redeem(address _token, uint256 _amount) internal returns (uint256) {
         IERC20(_token).safeTransfer(msg.sender, _amount);
-        IMintableContract(uniBTC).burnFrom(msg.sender, _amount);
+
+        uint256 uniBTCAmt = _uniBTCAmt(_token, _amount);
+        IMintableContract(uniBTC).burnFrom(msg.sender, uniBTCAmt);
         emit Redeemed(_token, _amount);
+
+        return uniBTCAmt;
+    }
+
+    /**
+     * @dev calculate the equivalent uniBTC amount to mint or burn, which conforms to the proportion `1 xBTC = 1 uniBTC`
+     */
+    function _uniBTCAmt(address _token, uint256 _amount) internal returns (uint256) {
+        uint256 r = _exchangeRatio(_token);
+        return _amount * r / MULTIPLIER;
+    }
+
+    /**
+     * @dev calculate the exchange ratio of xBTC (wrapped BTC) to uniBTC, which conforms to the proportion `1 xBTC = 1 uniBTC`
+     */
+    function _exchangeRatio(address _token) internal returns (uint256) {
+        uint256 uniBTCDecimals = ERC20(uniBTC).decimals();
+        uint256 xBTCDecimals = ERC20(_token).decimals();
+        return 10**uniBTCDecimals * MULTIPLIER / 10**xBTCDecimals;
     }
 
     /**
