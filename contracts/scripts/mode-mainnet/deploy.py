792c013aae3ab24be152c0336815e67ada57ef92
Trisome-Bedrock <trisome@bedrock-technology>
2024-09-10 17:08:49 +0800

    deployment on Mode mainnet

---
+++ contracts/scripts/mode-mainnet/deploy.py
@@ -0,0 +1,72 @@
+from brownie import uniBTC, Vault, accounts, Contract, project, config
+from pathlib import Path
+from web3 import Web3
+
+
+# Execution Command Format:
+# `brownie run scripts/mode-mainnet/deploy.py main "uniBTCMainnetDeployer" "uniBTCMainnetAdmin" --network=mode-main -I`
+
+
+def main(deployer="deployer", owner="owner"):
+    deps = project.load(  Path.home() / ".brownie" / "packages" / config["dependencies"][0])
+    TransparentUpgradeableProxy = deps.TransparentUpgradeableProxy
+    ProxyAdmin = deps.ProxyAdmin
+
+    w3 = Web3(Web3.HTTPProvider('http://localhost:8545'))
+    default_admin_role = w3.to_bytes(hexstr="0x00")
+    pauser_role = w3.keccak(text='PAUSER_ROLE')
+    minter_role = w3.keccak(text='MINTER_ROLE')
+
+    deployer = accounts.load(deployer)
+    owner = accounts.load(owner)
+
+    # Deploy ProxyAdmin
+    proxyAdmin = ProxyAdmin.deploy({'from': owner})
+
+    # Deploy uniBTC
+    uniBTC_impl = uniBTC.deploy({'from': deployer})
+    uniBTC_proxy = TransparentUpgradeableProxy.deploy(uniBTC_impl, proxyAdmin, b'', {'from': deployer})
+
+    # Deploy Vault
+    vault_impl = Vault.deploy({'from': deployer})
+    vault_proxy = TransparentUpgradeableProxy.deploy(vault_impl, proxyAdmin, b'', {'from': deployer})
+
+    # Initialize Vault
+    vault_transparent = Contract.from_abi("Vault", vault_proxy, Vault.abi)
+    vault_transparent.initialize(owner, uniBTC_proxy, {'from': owner})
+    assert vault_transparent.hasRole(default_admin_role, owner)
+    assert vault_transparent.hasRole(pauser_role, owner)
+    assert vault_transparent.uniBTC() == uniBTC_proxy
+
+    # Initialize uniBTC
+    uniBTC_transparent = Contract.from_abi("uniBTC", uniBTC_proxy, uniBTC.abi)
+    uniBTC_transparent.initialize(owner, vault_proxy, {'from': owner})
+    assert uniBTC_transparent.hasRole(default_admin_role, owner)
+    assert uniBTC_transparent.hasRole(minter_role, vault_proxy)
+
+    # Set WBTC cap
+    wbtc = "0xcDd475325D6F564d27247D1DddBb0DAc6fA0a5CF"
+    cap = 5000 * 1e8
+    vault_transparent.setCap(wbtc, cap, {'from':owner})
+    assert vault_transparent.caps(wbtc) == cap
+
+    # Set M-BTC cap
+    mbtc = "0x59889b7021243dB5B1e065385F918316cD90D46c"
+    cap = 5000 * 1e18
+    vault_transparent.setCap(mbtc, cap, {'from':owner})
+    assert vault_transparent.caps(mbtc) == cap
+
+
+    print("Deployed ProxyAdmin address: ", proxyAdmin)  # 0xb3f925B430C60bA467F7729975D5151c8DE26698
+    print("Deployed uniBTC proxy address: ", uniBTC_proxy)  # 0x6B2a01A5f79dEb4c2f3c0eDa7b01DF456FbD726a
+    print("Deployed Vault proxy address: ", vault_proxy)    # 0x84E5C854A7fF9F49c888d69DECa578D406C26800
+
+    print("")
+
+    print("Deployed uniBTC implementation address: ", uniBTC_impl)  # 0x94C7F81E3B0458daa721Ca5E29F6cEd05CCCE2B3
+    print("Deployed Vault implementation address: ", vault_impl)    # 0x08cB45f7FC43C25BbE830DacFe57D72CbC46775d
+
+
+
+
+
