d23285c4ea372fb7e4363990ba5a0283a092e802
Trisome-Bedrock <trisome@bedrock-technology>
2024-09-23 19:30:48 +0800

    deploy Sigma on BOB

---
+++ contracts/scripts/bob-mainnet/deploy-Sigma.py
@@ -0,0 +1,49 @@
+from brownie import Sigma, accounts, Contract, project, config
+from pathlib import Path
+from web3 import Web3
+
+
+# Execution Command Format:
+# `brownie run scripts/bob-mainnet/deploy-Sigma.py main "uniBTCMainnetDeployer" "uniBTCMainnetAdmin" --network=bob-mainnet -I`
+
+
+def main(deployer="deployer", owner="owner"):
+    deps = project.load(  Path.home() / ".brownie" / "packages" / config["dependencies"][0])
+    TransparentUpgradeableProxy = deps.TransparentUpgradeableProxy
+
+    w3 = Web3(Web3.HTTPProvider('http://localhost:8545'))
+    default_admin_role = w3.to_bytes(hexstr="0x00")
+
+    deployer = accounts.load(deployer)
+    owner = accounts.load(owner)
+
+    # Deployed core contracts
+    proxy_admin = "0x56c3024eB229Ca0570479644c78Af9D53472B3e4"
+    vault = "0x2ac98DB41Cbd3172CB7B8FD8A8Ab3b91cFe45dCf"
+
+    # Deploy and initialize Sigma
+    sigma_impl = Sigma.deploy({'from': deployer})
+    initialize_data = sigma_impl.initialize.encode_input(owner)
+    sigma_proxy = TransparentUpgradeableProxy.deploy(sigma_impl, proxy_admin, initialize_data, {'from': deployer})
+
+    # Check initial status
+    sigma_transparent = Contract.from_abi("Sigma", sigma_proxy, Sigma.abi)
+    assert sigma_transparent.hasRole(default_admin_role, owner)
+
+    # Set holders of WBTC, which have 8 decimals.
+    wbtc = "0x03C7054BCB39f7b2e5B2c7AcB37583e32D70Cfa3"
+    wbtc_pools = [
+        (wbtc, (vault,))
+    ]
+    tx = sigma_transparent.setTokenHolders(wbtc, wbtc_pools, {'from': owner})
+    assert "TokenHoldersSet" in tx.events
+    assert sigma_transparent.getTokenHolders(wbtc) == wbtc_pools
+    assert len(sigma_transparent.ListLeadingTokens()) == 2
+
+    # Check supply of WBTC
+    WBTC = deps.ERC20.at(wbtc)
+    assert sigma_transparent.totalSupply(wbtc) == WBTC.balanceOf(vault)
+
+    print("Deployed Sigma proxy address: ", sigma_proxy)  # 0x94C7F81E3B0458daa721Ca5E29F6cEd05CCCE2B3
+    print("")
+    print("Deployed Sigma implementation address: ", sigma_impl)  # 0x12073748B427D2BB7064c3dF120ee04448AA29a0
