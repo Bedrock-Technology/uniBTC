271784e655a0239fb4a7fb07b579ebe0f363bfc1
eben.xie <xiegang@rockx.com>
2024-09-30 14:39:13 +0800

    [Describe]: support fbtc_proxy rbac feature

--- contracts/contracts/proxies/FBTCProxy.sol
+++ contracts/contracts/proxies/FBTCProxy.sol
@@ -1,20 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import "../../interfaces/IVault.sol";
 import "../../interfaces/ILockedFBTC.sol";
 
 // Reference: https://github.com/fbtc-com/fbtcX-contract/blob/main/src/LockedFBTC.sol
-contract FBTCProxy is Ownable {
+contract FBTCProxy is AccessControl {
     address public immutable vault;
     address public immutable lockedFBTC;
+    bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
 
-    constructor(address _vault, address _lockedFBTC) {
+    constructor(address _vault, address _lockedFBTC, address _admin) {
         vault = _vault;
         lockedFBTC = _lockedFBTC;
+        _grantRole(DEFAULT_ADMIN_ROLE, _admin);
+        _grantRole(OPERATOR_ROLE, _admin);
     }
 
     receive() external payable {
@@ -24,7 +27,7 @@ contract FBTCProxy is Ownable {
     /**
      * @dev  mint lockedFBTC in response to a burn FBTC request on the FBTC bridge.
      */
-    function mintLockedFbtcRequest(uint256 _amount) external onlyOwner {
+    function mintLockedFbtcRequest(uint256 _amount) external onlyRole(OPERATOR_ROLE) {
         address fbtc = ILockedFBTC(lockedFBTC).fbtc();
         if (IERC20(fbtc).allowance(vault, lockedFBTC) < _amount) {
             bytes memory data = abi.encodeWithSelector(IERC20.approve.selector, lockedFBTC, _amount);
@@ -38,7 +41,7 @@ contract FBTCProxy is Ownable {
     /**
      * @dev initiate a FBTC redemption request on the FBTC bridge with the corresponding BTC deposit tx details.
      */
-    function redeemFbtcRequest(uint256 _amount, bytes32 _depositTxid, uint256 _outputIndex) external onlyOwner {
+    function redeemFbtcRequest(uint256 _amount, bytes32 _depositTxid, uint256 _outputIndex) external onlyRole(OPERATOR_ROLE) {
         bytes memory data = abi.encodeWithSelector(ILockedFBTC.redeemFbtcRequest.selector, _amount, _depositTxid, _outputIndex);
         IVault(vault).execute(lockedFBTC, data, 0);
     }
@@ -46,16 +49,8 @@ contract FBTCProxy is Ownable {
     /**
      * @dev burn Vault's locked FBTC and transfer back the equivalent FBTC to Vault.
      */
-    function confirmRedeemFbtc(uint256 _amount) external onlyOwner {
+    function confirmRedeemFbtc(uint256 _amount) external onlyRole(OPERATOR_ROLE) {
         bytes memory data = abi.encodeWithSelector(ILockedFBTC.confirmRedeemFbtc.selector, _amount);
-        IVault(vault).execute(lockedFBTC, data, 0);
-    }
-
-    /**
-     * @dev burn Vault's lockedFBTC
-     */
-    function burn(uint256 _amount) external onlyOwner {
-        bytes memory data = abi.encodeWithSelector(ILockedFBTC.burn.selector, _amount);
         IVault(vault).execute(lockedFBTC, data, 0);
     }
 }
--- contracts/tests/test_FBTCProxy.py
+++ contracts/tests/test_FBTCProxy.py
@@ -3,7 +3,7 @@ import brownie
 from brownie import accounts
 
 
-def test_mintLockedFbtcRequest(fn_isolation, contracts, owner, alice):
+def test_mintLockedFbtcRequest(fn_isolation, contracts, owner, alice, executor):
     uni_btc, vault, fbtc, fbtc_proxy, locked_fbtc = contracts[0], contracts[6], contracts[7], contracts[9], contracts[10]
 
     amt =  1e10
@@ -16,7 +16,7 @@ def test_mintLockedFbtcRequest(fn_isolation, contracts, owner, alice):
     vault.grantRole(vault.OPERATOR_ROLE(), fbtc_proxy, {'from': owner})
 
     # User mints FBTC
-    fbtc.mint(alice, amt, {'from': owner})
+    fbtc.mint(alice, amt, {'from': executor})
     assert fbtc.balanceOf(alice) == amt
 
     # User deposits FBTC
@@ -29,13 +29,13 @@ def test_mintLockedFbtcRequest(fn_isolation, contracts, owner, alice):
     assert uni_btc.balanceOf(alice) == amt
 
     # Vault mints LockedFBTC
-    tx = fbtc_proxy.mintLockedFbtcRequest(amt, {'from': owner})
+    tx = fbtc_proxy.mintLockedFbtcRequest(amt, {'from': executor})
     assert 'MintLockedFbtcRequest' in tx.events
     assert locked_fbtc.balanceOf(vault) == real_amt
     assert fbtc.balanceOf(vault) == 0
 
 
-def test_redeemFbtcRequest(fn_isolation, contracts, owner, alice):
+def test_redeemFbtcRequest(fn_isolation, contracts, owner, alice, executor):
     uni_btc, vault, fbtc, fbtc_proxy, locked_fbtc = contracts[0], contracts[6], contracts[7], contracts[9], contracts[10]
 
     amt = 1e10
@@ -48,7 +48,7 @@ def test_redeemFbtcRequest(fn_isolation, contracts, owner, alice):
     vault.grantRole(vault.OPERATOR_ROLE(), fbtc_proxy, {'from': owner})
 
     # User mints FBTC
-    fbtc.mint(alice, amt, {'from': owner})
+    fbtc.mint(alice, amt, {'from': executor})
     assert fbtc.balanceOf(alice) == amt
 
     # User deposits FBTC
@@ -61,18 +61,18 @@ def test_redeemFbtcRequest(fn_isolation, contracts, owner, alice):
     assert uni_btc.balanceOf(alice) == amt
 
     # Vault mints LockedFBTC
-    tx = fbtc_proxy.mintLockedFbtcRequest(amt, {'from': owner})
+    tx = fbtc_proxy.mintLockedFbtcRequest(amt, {'from': executor})
     assert 'MintLockedFbtcRequest' in tx.events
     assert locked_fbtc.balanceOf(vault) == real_amt
     assert fbtc.balanceOf(vault) == 0
 
     # Send redeem request
     fake_tx_id = '0x' + ''.join(random.choices('0123456789abcdef', k=64))
-    tx = fbtc_proxy.redeemFbtcRequest(real_amt, fake_tx_id, 0, {'from': owner})
+    tx = fbtc_proxy.redeemFbtcRequest(real_amt, fake_tx_id, 0, {'from': executor})
     assert 'RedeemFbtcRequest' in tx.events
 
 
-def test_confirmRedeemFbtc(fn_isolation, contracts, owner, alice):
+def test_confirmRedeemFbtc(fn_isolation, contracts, owner, alice, executor):
     uni_btc, vault, fbtc, fbtc_proxy, locked_fbtc = contracts[0], contracts[6], contracts[7], contracts[9], contracts[10]
 
     amt = 1e10
@@ -85,7 +85,7 @@ def test_confirmRedeemFbtc(fn_isolation, contracts, owner, alice):
     vault.grantRole(vault.OPERATOR_ROLE(), fbtc_proxy, {'from': owner})
 
     # User mints FBTC
-    fbtc.mint(alice, amt, {'from': owner})
+    fbtc.mint(alice, amt, {'from': executor})
     assert fbtc.balanceOf(alice) == amt
 
     # User deposits FBTC
@@ -98,64 +98,18 @@ def test_confirmRedeemFbtc(fn_isolation, contracts, owner, alice):
     assert uni_btc.balanceOf(alice) == amt
 
     # Vault mints LockedFBTC
-    tx = fbtc_proxy.mintLockedFbtcRequest(amt, {'from': owner})
+    tx = fbtc_proxy.mintLockedFbtcRequest(amt, {'from': executor})
     assert 'MintLockedFbtcRequest' in tx.events
     assert locked_fbtc.balanceOf(vault) == real_amt
     assert fbtc.balanceOf(vault) == 0
 
     # Send redeem request
     fake_tx_id = '0x' + ''.join(random.choices('0123456789abcdef', k=64))
-    tx = fbtc_proxy.redeemFbtcRequest(real_amt, fake_tx_id, 0, {'from': owner})
+    tx = fbtc_proxy.redeemFbtcRequest(real_amt, fake_tx_id, 0, {'from': executor})
     assert 'RedeemFbtcRequest' in tx.events
 
     # Confirm redeem request
-    tx = fbtc_proxy.confirmRedeemFbtc(real_amt, {'from': owner})
+    tx = fbtc_proxy.confirmRedeemFbtc(real_amt, {'from': executor})
     assert 'ConfirmRedeemFbtc' in tx.events
     assert locked_fbtc.balanceOf(vault) == 0
     assert fbtc.balanceOf(vault) == real_amt
-
-def test_burn(fn_isolation, contracts, owner, alice):
-    uni_btc, vault, fbtc, fbtc_proxy, locked_fbtc = contracts[0], contracts[6], contracts[7], contracts[9], contracts[10]
-
-    amt = 1e10
-    fee = locked_fbtc.fee()
-    real_amt = amt - fee
-
-    # Configuration
-    vault.setCap(fbtc, amt*10, {'from': owner})
-
-    vault.grantRole(vault.OPERATOR_ROLE(), fbtc_proxy, {'from': owner})
-
-    # User mints FBTC
-    fbtc.mint(alice, amt, {'from': owner})
-    assert fbtc.balanceOf(alice) == amt
-
-    # User deposits FBTC
-    fbtc.approve(vault, amt, {'from': alice})
-    assert fbtc.allowance(alice, vault) == amt
-
-    vault.mint(fbtc, amt, {'from': alice})
-    assert fbtc.balanceOf(vault) == amt
-    assert fbtc.balanceOf(alice) == 0
-    assert uni_btc.balanceOf(alice) == amt
-
-    # Vault mints LockedFBTC
-    tx = fbtc_proxy.mintLockedFbtcRequest(amt, {'from': owner})
-    assert 'MintLockedFbtcRequest' in tx.events
-    assert locked_fbtc.balanceOf(vault) == real_amt
-    assert fbtc.balanceOf(vault) == 0
-
-    # Send redeem request
-    fake_tx_id = '0x' + ''.join(random.choices('0123456789abcdef', k=64))
-    tx = fbtc_proxy.redeemFbtcRequest(real_amt, fake_tx_id, 0, {'from': owner})
-    assert 'RedeemFbtcRequest' in tx.events
-
-#     # Confirm redeem request
-#     tx = fbtc_proxy.confirmRedeemFbtc(real_amt, {'from': owner})
-#     assert 'ConfirmRedeemFbtc' in tx.events
-#     assert locked_fbtc.balanceOf(vault) == 0
-#     assert fbtc.balanceOf(vault) == real_amt
-
-    # Burn request
-    tx = fbtc_proxy.burn(real_amt, {'from': owner})
-    assert locked_fbtc.balanceOf(vault) == 0
