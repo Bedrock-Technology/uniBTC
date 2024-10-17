2a81116ce74ae649c3274c28b4dc20e65e17a362
Jun Luo <jun@rockx.com>
2024-10-17 12:18:53 +0800

    mainnet deploy script: directBTC

--- contracts/scripts/directbtc-mainnet/deploy.py
+++ contracts/scripts/directbtc-mainnet/deploy.py
@@ -4,24 +4,19 @@ from web3 import Web3
 
 
 # Execution Command Format:
-# `brownie run scripts/testnet/deploy_directBTC_holesky.py main "testnet-deployer" "testnet-owner" --network=holesky -I`
+# `brownie run scripts/directbtc-mainnet/deploy.py main "mainnet-deployer" "mainnet-owner" --network=mainnet -I`
 
-def main(deployer="testnet-deployer", owner="testnet-owner"):
+def main(deployer="mainnet-deployer", owner="mainnet-owner"):
 
     # make sure to set the correct ProxyAdmin and uniBtcVault address
-    proxyAdmin = '0xC0c9E78BfC3996E8b68D872b29340816495D7e89'
-    uniBtcVault = '0x97e16DB82E089D0C9c37bc07F23FcE98cfF04823'
+    proxyAdmin = '0x029E4FbDAa31DE075dD74B2238222A08233978f6'
+    uniBtcVault = '0x047D41F2544B7F63A8e991aF2068a363d210d6Da'
 
     deps = project.load(Path.home() / ".brownie" / "packages" / config["dependencies"][0])
     TransparentUpgradeableProxy = deps.TransparentUpgradeableProxy
 
     w3 = Web3(Web3.HTTPProvider('http://localhost:8545'))
     default_admin_role = w3.to_bytes(hexstr="0x00")
-    # directBTC roles
-    minter_role = w3.keccak(text='MINTER_ROLE')
-    # directBTCMinter roles
-    approver_role = w3.keccak(text='APPROVER_ROLE')
-    l1minter_role = w3.keccak(text='L1_MINTER_ROLE')
 
     deployer = accounts.load(deployer)
     owner = accounts.load(owner)
@@ -37,6 +32,9 @@ def main(deployer="testnet-deployer", owner="testnet-owner"):
     # vault proxy
     vault_proxy = Contract.from_abi("Vault", uniBtcVault, Vault.abi)
 
+    # directBTCMinter roles
+    approver_role = w3.keccak(text='APPROVER_ROLE')
+    l1minter_role = w3.keccak(text='L1_MINTER_ROLE')
     # init minter
     minter_transparent = Contract.from_abi("DirectBTCMinter", minter_proxy, DirectBTCMinter.abi)
     minter_transparent.initialize(owner, directBTC_proxy, uniBtcVault, vault_proxy.uniBTC(), {'from': owner})
@@ -45,12 +43,13 @@ def main(deployer="testnet-deployer", owner="testnet-owner"):
     assert minter_transparent.hasRole(approver_role, owner)
     assert minter_transparent.directBTC() == directBTC_proxy
 
+    # directBTC roles
+    minter_role = w3.keccak(text='MINTER_ROLE')
     # init directBTC
     transparent_directBTC = Contract.from_abi("directBTC", directBTC_proxy, directBTC.abi)
     transparent_directBTC.initialize(owner, minter_proxy, {'from': owner})
     assert transparent_directBTC.hasRole(default_admin_role, owner)
     assert transparent_directBTC.hasRole(minter_role, minter_proxy)
-
 
     print("| Contract                     | Address                                    |")
     print("|------------------------------|--------------------------------------------|")
@@ -63,3 +62,5 @@ def main(deployer="testnet-deployer", owner="testnet-owner"):
     print("| DirectBTCMinter proxy        |", minter_proxy, "|")
     print("| DirectBTCMinter imple        |", minter_impl, "|")
     print("|------------------------------|--------------------------------------------|")
+
+
--- directBTC.md
+++ directBTC.md
@@ -9,6 +9,33 @@
   
   The custodian wallet is managed by a third party.
 
+## deploy contract: holesky
+
+  | Contract                     | Address                                    |
+  |------------------------------|--------------------------------------------|
+  | ProxyAdmin                   | 0xC0c9E78BfC3996E8b68D872b29340816495D7e89 |
+  | uniBtc Vault                 | 0x97e16DB82E089D0C9c37bc07F23FcE98cfF04823 |
+  |------------------------------|--------------------------------------------|
+  | directBTC proxy              | 0xe3747f26A74E4831Cdd0f3E54733B379D7842c7A |
+  | directBTC imple              | 0x647Dc65c8DFb6f1a09C0A1f9F56AEed941ef2277 |
+  |------------------------------|--------------------------------------------|
+  | DirectBTCMinter proxy        | 0x0F433A202611Afa304B873D31149731Bd746a943 |
+  | DirectBTCMinter imple        | 0xD4c9C929CE4904D8b79ad1734f69777feFF51af7 |
+
+## deploy contract: mainnet
+<!-- 
+| Contract                     | Address                                    |
+|------------------------------|--------------------------------------------|
+| ProxyAdmin                   | 0x029E4FbDAa31DE075dD74B2238222A08233978f6 |
+| uniBtcVault                  | 0x047D41F2544B7F63A8e991aF2068a363d210d6Da |
+|------------------------------|--------------------------------------------|
+| directBTC proxy              | 0x290ae25790112D619EEF07E878674D7AAbe9CbF0 |
+| directBTC imple              | 0xeDd7E3DF86E80E5237bFa2436fE4575733CD6533 |
+|------------------------------|--------------------------------------------|
+| DirectBTCMinter proxy        | 0xF2EB1dc3cE6bf85AbB089101642CFC92a6bAB931 |
+| DirectBTCMinter imple        | 0x5760c5050ffE04703804C31624B1C6969a5B455e |
+|------------------------------|--------------------------------------------| -->
+
 ### ERROR code from contract
 
 - SYS001: INVALID_ADDRESS
