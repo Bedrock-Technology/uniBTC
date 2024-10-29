58b886ef3a81fd08a0641ed71d6739025257cb2e
MarcusBright <maryhasapoorlamb@hotmail.com>
2024-10-29 14:29:40 +0800

    2.3.1 Remove unused contract inheritance

--- contracts/contracts/BR.sol
+++ contracts/contracts/BR.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract Bedrock is ERC20, ERC20Burnable, AccessControl {
+contract Bedrock is ERC20Burnable, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     constructor(address defaultAdmin, address minter) ERC20("Bedrock", "BR") {
@@ -17,4 +16,3 @@ contract Bedrock is ERC20, ERC20Burnable, AccessControl {
         _mint(to, amount);
     }
 }
-
