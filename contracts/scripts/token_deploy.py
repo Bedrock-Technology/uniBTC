d83524ec1799d32edfb217b83bd04abae1a68ede
johnbedrock <166798358+johnbedrock@users.noreply.github.com>
2024-11-16 18:56:45 +0800

    added taiko deployment

--- contracts/scripts/token_deploy.py
+++ contracts/scripts/token_deploy.py
@@ -11,8 +11,9 @@ def main():
     owner = accounts.load('mainnet-owner')
     deployer = accounts.load('mainnet-deployer')
 
-    uniBTC_impl = uniBTC.deploy({'from': deployer})
+    #uniBTC_impl = uniBTC.deploy({'from': deployer})
+    uniBTC_impl ="0x552b0C6688FCaE5cF0164F27Fd129b882a42fA05"
     uniBTC_proxy = TransparentUpgradeableProxy.deploy(uniBTC_impl, deployer, b'', {'from': deployer})
 
     transparent_uniBTC = Contract.from_abi("uniBTC",uniBTC_proxy.address, uniBTC.abi)
-    transparent_uniBTC.initialize(owner, owner, {'from': owner})
+    transparent_uniBTC.initialize(owner, owner, [], {'from': owner})
